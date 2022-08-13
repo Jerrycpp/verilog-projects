e3cde9c1ad3eca0ca16e349f821be3f78c9000c5
Jerry Hoang <jerryhoang.kaka1901@gmail.com>
2022-08-13 10:04:56 +0700

    update on i2c fsm

--- src/hdl/i2c_fsm.v
+++ src/hdl/i2c_fsm.v
@@ -5,7 +5,10 @@ module i2c_fsm (
     output out;
     reg [1:0] state, next_state;
     reg [6:0] address;
+    reg [7:0] data; // Data or cmd
     reg rw;
+    reg rdy; // next bit Ready to go?
+    reg [4:0] counter;
     wire start, stop;
     localparam IDLE = 0, START = 2'd1, READ = 2'd2, WRITE = 2'd3;
     edge_detector start_stop_detect (.clk(clk), .sda(sda), .posedge_out(stop), .negedge_out(start), .rst(rst));
@@ -13,12 +16,17 @@ module i2c_fsm (
         if (rst) begin
             state <= IDLE;
             rw <= 1'bz;
+            address <= 7'b0000000;
+            data <= 8'b00000000;
+            rdy <= 0;
+            counter <= 4'0000;
         end
         else begin
             state <= next_state;
         end
     end
 
+    //handle state
     always @* begin
         next_state = state;
         case (state) 
@@ -47,6 +55,30 @@ module i2c_fsm (
         endcase
     end
     
+
+    //Wrting mode
+    always @ (posedge clk) begin
+        if (state == START) begin
+            if (counter < 4'd7) begin
+                if (scl == 1 && rdy == 1) begin
+                    address <= {address[6:1], inp};
+                    rdy <= 0;
+                    counter <= counter + 1;
+                end 
+                else if (scl == 0) begin
+                    rdy <= 1;
+                end
+            end
+            else if (counter == 4'd7) begin
+                counter <= 4'd0;
+                if (scl == 1 && rdy == 1) begin
+                    rw <= inp;
+                end
+            end
+        end
+    end
+
+
 
 
     
