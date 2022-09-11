d88c746ef9137bcf5d124a6adf47e064152c82fa
Jerry Hoang <jerryhoang.kaka1901@gmail.com>
2022-09-11 09:51:20 +0700

    dar

--- src/hdl/dar.v
+++ src/hdl/dar.v
@@ -1,10 +1,10 @@
 module dar (clk, rst, data_in, data_out, w_en, r_en, addr);
     input clk, rst, w_en, r_en;
     input [7:0] data_in;
-    input [6:0] addr;
+    input [1:0] addr;
     output [7:0] data_out;
     integer i;
-    reg [7:0] regs [127:0];
+    reg [7:0] regs [3:0];
 
     always @ (posedge clk or posedge rst) begin
         if (rst) begin
@@ -23,4 +23,5 @@ module dar (clk, rst, data_in, data_out, w_en, r_en, addr);
     end
 
 
-endmodule;
+
+endmodule
