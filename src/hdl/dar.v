9609e7ed6ad5cea73a97c961684fc93edba67dce
Jerry Hoang <jerryhoang.kaka1901@gmail.com>
2022-09-13 08:36:27 +0700

    dar 8 bits 128 regs

--- src/hdl/dar.v
+++ src/hdl/dar.v
@@ -1,14 +1,14 @@
 module dar (clk, rst, data_in, data_out, w_en, r_en, addr);
     input clk, rst, w_en, r_en;
     input [7:0] data_in;
-    input [1:0] addr;
+    input [6:0] addr;
     output [7:0] data_out;
     integer i;
-    reg [7:0] regs [3:0];
+    reg [7:0] regs [127:0];
 
     always @ (posedge clk or posedge rst) begin
         if (rst) begin
-            for (i = 0 ; i < 4 ; i = i+1) begin
+            for (i = 0 ; i < 128 ; i = i+1) begin
                 regs[i] <= 8'b0;
             end
         end
